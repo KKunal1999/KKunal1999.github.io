902cadaa887f81b122b0d65946317ddf88d25746
Kishor Kunal <kishorkunal1999@gmail.com>
2023-05-05 16:49:18 +0530

    gradebook project

---
+++ gradebook/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Header,Footer} from './components/HeaderFooter';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Footer/>
+      <Main/>
+    </div>
+  );
+}
+export default App;
